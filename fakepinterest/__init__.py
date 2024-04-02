4df64242cd2e20f407796aa86855d2d7108031ba
Marcondes <marcondesgoncalves159@gmail.com>
2024-04-01 22:42:19 -0300

    Criando o banco de dados e o Model Usuario e Foto

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,8 +1,13 @@
 # Criação do App tem que ser feita em um arquivo com o nome __init__
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# AQUI DEFINIMOS QUAL BANCO IREMOS CRIAR
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
+database = SQLAlchemy(app)
 
 
 # As importações necessárias de outros módulos tem que ser feita após a criação ao App
--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,1 +1,31 @@
 # Criar a estrutura do banco de dados
+from fakepinterest import database
+from datetime import datetime
+
+# database.Model: com esse parâmatro conseguiremos transformar a classe em uma tabela e uma tabela em uma classe.
+class Usuario(database.Model):
+
+    # primary_key = True O ID SERÁ INCREMENTADO AUTOMÁTICAMENTE A CADA NOVO USUÁRIO
+    id: int = database.Column(database.Integer, primary_key = True)
+
+    # nullable = False SIGNIFICA QUE ESSES CAMPOS NÃO PODEM SER NULLOS
+    username: str = database.Column(database.String, nullable = False)
+    senha: str = database.Column(database.String, nullable = False)
+
+    # O usuário só proderá criar uma conta por email
+    email: str = database.Column(database.String, nullable = False, unique = True)
+
+    # fotos não vai ser uma  coluna, mas sim uma referência para classe Foto.
+    fotos = database.relationship("Foto", backref = "usuario", lazy = True)
+
+
+class Foto(database.Model):
+
+    id: int = database.Column(database.Integer, primary_key = True)
+    imagem = database.Column(database.String, default = "default.png")
+
+    data_criacao = database.Column(database.DateTime, nullable = False, default = datetime.utcnow())
+
+    # chave estrangeira que faz referência ao usuário   # type: ignore[Unknown]
+    # note que a classe tem que ser passada em minusculas, pois no banco a mesma será criada com minusculas. 
+    id_usuario: int = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable = False) 
