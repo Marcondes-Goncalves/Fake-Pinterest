5a7ae207c46626a2de775229673f3e521139228a
Marcondes <marcondesgoncalves159@gmail.com>
2024-04-02 14:14:02 -0300

    Implementando o formulário de login e de criar conta

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,15 +1,24 @@
 # Criar as rotas do nosso site (LINKS)
 
 # url_for PERMITE QUE VOCÊ PEGUE A URL DE ACORDO COM O NOME DA FUNÇÃO E NÃO DE ACORDO COM A ROTA
-from flask import render_template, url_for
-from fakepinterest import app
+from flask import render_template
 from flask_login import login_required
+
+from fakepinterest import app
+from fakepinterest.forms import FormLogin, FormCriarConta
 
 
 # Definindo a rota do nosso site
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form = formlogin)
+
+
+@app.route("/criarconta", methods = ["GET", "POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form = formcriarconta)
 
 
 @app.route("/perfil/<usuario>") # <usuario> SIGNIFICA QUE A TAG USUÁRIO AGORA É UMA VARIÁVEL
